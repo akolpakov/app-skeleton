0168185f2e775ccfcda6e5e1cdb44ad0f5ecd520
Andrey Kolpakov <aakolpakov@gmail.com>
2017-12-13 11:06:18 +0100

    Update to the latest `d2-ui` lib version #7

--- src/app/App.js
+++ src/app/App.js
@@ -1,39 +1,35 @@
 import React from 'react';
 import log from 'loglevel';
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import HeaderBarComponent from 'd2-ui/lib/app-header/HeaderBar';
 import headerBarStore$ from 'd2-ui/lib/app-header/headerBar.store';
 import withStateFrom from 'd2-ui/lib/component-helpers/withStateFrom';
 
 import Sidebar from 'd2-ui/lib/sidebar/Sidebar.component';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
 const HeaderBar = withStateFrom(headerBarStore$, HeaderBarComponent);
 
-export default React.createClass({
-    propTypes: {
-        name: React.PropTypes.string,
-        d2: React.PropTypes.object,
-    },
 
-    childContextTypes: {
-        d2: React.PropTypes.object,
-    },
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
-    getDefaultProps() {
-        return {
-            name: 'John',
-        };
-    },
+// Application
 
+class App extends React.Component {
     getChildContext() {
         return {
             d2: this.props.d2,
         };
-    },
+    }
 
     _sidebarItemClicked(sideBarItemKey) {
         log.info('Clicked on ', sideBarItemKey);
-    },
+    }
 
     render() {
         const sideBarSections = [
@@ -42,14 +38,31 @@ export default React.createClass({
         ];
 
         return (
-            <div className="app-wrapper">
-                <HeaderBar />
-                <Sidebar
-                    sections={sideBarSections}
-                    onChangeSection={this._sidebarItemClicked}
-                />
-                <div className="main-content">{`Hello, ${this.props.name}! Your app skeleton set up correctly!`}</div>
-            </div>
+            <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <div className="app-wrapper">
+                    <HeaderBar />
+                    <Sidebar
+                        sections={sideBarSections}
+                        onChangeSection={this._sidebarItemClicked}
+                    />
+                    <div className="main-content">{`Hello, ${this.props.name}! Your app skeleton set up correctly!`}</div>
+                </div>
+            </MuiThemeProvider>
         );
-    },
-});
+    }
+}
+
+App.childContextTypes = {
+    d2: React.PropTypes.object,
+    name: React.PropTypes.string,
+};
+
+App.childContextTypes = {
+    d2: React.PropTypes.object,
+};
+
+App.defaultProps = {
+    name: 'John'
+};
+
+export default App;
